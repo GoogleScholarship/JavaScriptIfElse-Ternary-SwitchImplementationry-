64fedfe0d13d3f1dc041508414fae5268bf1baab
Ryan Murphy <murjax@gmail.com>
2018-02-07 18:08:19 -0500

    Refactored

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 var num = 60;
+var test;
+while (num >=0){
+  test = testy(num);
+  console.log(test);
+  num--;
+}
 
-for (var num = 60; num >=0; num--){
-    switch(num){
-            case 50: console.log("Orbiter transfers from ground to internal power");
-            break;
-            case 31: console.log("Orbiter transfers from ground to internal power");
-            break;
-            case 16: console.log("Orbiter transfers from ground to internal power");
-            break;
-            case 10: console.log("Orbiter transfers from ground to internal power");
-            break;
-            case 6: console.log("Orbiter transfers from ground to internal power");
-            break;
-            case 0: console.log("Orbiter transfers from ground to internal power");
-            break;
-            default: console.log("T-"+num+" seconds");
-    }
+function testy (num) {
+  switch(num) {
+    case 50:return ("Orbiter transfers from ground to internal power");
+    case 31:return ("Ground launch sequencer is go for auto sequence start");
+    case 16:return ("Activate launch pad sound suppression system");
+    case 10:return ("Main engine start");
+    case 6: return ("Solid rocket booster ignition and liftoff!");
+    case 0: return ("Solid rocket booster ignition and liftoff!");
+    default: return ("T-"+num+" seconds");
+  }
 }
