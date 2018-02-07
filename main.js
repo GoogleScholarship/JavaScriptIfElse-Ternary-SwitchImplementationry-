d1c3c057c110e5150954bbdf33325ca07427dbc0
americanman <snowboardracer7@gmail.com>
2018-02-06 21:08:32 -0500

    my quick version of swith

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-var num = 60; 
+var num = 60;
 
-while (num >=0){
-  if (num === 50) {
-    console.log("Orbiter transfers from ground to internal power");
-  }else if (num === 31) {
-    console.log("Ground launch sequencer is go for auto sequence start");
-  }else if (num === 16) {
-    console.log("Activate launch pad sound suppression system");
-  }else if (num === 10) {
-    console.log("Activate main engine hydrogen burnoff system");
-  }else if (num === 6) {
-    console.log ("Main engine start"); 
-  }else if (num === 0) {
-    console.log("Solid rocket booster ignition and liftoff!");
-  }else {
-    console.log("T-"+num+" seconds");
-  } 
-  num--;
+for (var num = 60; num >=0; num--){
+    switch(num){
+            case 50: console.log("Orbiter transfers from ground to internal power");
+            break;
+            case 31: console.log("Orbiter transfers from ground to internal power");
+            break;
+            case 16: console.log("Orbiter transfers from ground to internal power");
+            break;
+            case 10: console.log("Orbiter transfers from ground to internal power");
+            break;
+            case 6: console.log("Orbiter transfers from ground to internal power");
+            break;
+            case 0: console.log("Orbiter transfers from ground to internal power");
+            break;
+            default: console.log("T-"+num+" seconds");
+    }
 }
